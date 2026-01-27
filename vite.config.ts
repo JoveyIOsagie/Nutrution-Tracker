97109ca4e8e984e6e2b64eb4f03297c58278eae8
IoanaGaganelova <ioanagaganelova@gmail.com>
2026-01-26 22:47:05 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,1 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  resolve: {
-    alias: {
-      "@": "/src",
-    },
-  },
-  server: {
-    port: 5173,
-    open: true,
-  },
-});
